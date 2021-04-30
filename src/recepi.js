2b2f46963739b50fb9a68b03d30c3234e4d61bb4
lakshanamal <lakshanamal100@gmail.com>
2021-04-30 22:46:00 +0530

    responsiveness added

--- src/recepi.js
+++ src/recepi.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Recipe =({title,img})=>{
-    return (
-        <div className="recipe">
-            <img src={img} alt=""/>
-            <h3>{title}</h3>
-        </div>
-    )
+const Recipe = ({ title, img }) => {
+  return (
+    <div className="recipe">
+      <div className="recipe-content">
+        <img src={img} alt="" />
+        <h3>{title}</h3>
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default Recipe
+export default Recipe;
